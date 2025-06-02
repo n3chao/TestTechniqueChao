3eb0cd0109cc9ec4a7d663225e764a2883a2b097
Nick-KarlChao <nick-karl.chao.1@ens.etsmtl.ca>
2025-06-02 09:50:05 -0400

    Recording works, need timestamp+stockage+typescript

--- AudioRecorder/app/index.tsx
+++ AudioRecorder/app/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View, Pressable, StyleSheet, FlatList } from "react-native";
+import { Text, View, Pressable, StyleSheet, FlatList, ListRenderItem, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
+import { Audio } from 'expo-av';
 
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -10,31 +11,84 @@ import { audioFiles } from "@/data/audioFiles"
 export default function Index() {
   const [recording, setRecording] = useState();
   //const [recordings, setRecordings] = useState<any[]>([]);
-  const [recordings, setRecordings] = useState<any[]>(audioFiles.sort((a, b) => b.id - a.id));
+  const [recordingList, setRecordingList] = useState<any[]>(audioFiles.sort((a, b) => b.id - a.id));
 
+  async function startRecording() {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        const { recording } = await Audio.Recording.createAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+        setRecording(recording);
+      }
+    } catch (err) { }
 
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+    let allRecordings = [...recordingList];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    allRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    setRecordingList(allRecordings);
+  }
+
+  async function getDurationFormatted(milliseconds: number) {
+    const minutes = milliseconds / 1000 / 60;
+    const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
+    return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`
+
+  }
+
+  async function getRecordingLines() {
+    return recordingList.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.recordingListed}>
+          <Text style={styles.fill}>
+            Recording #{index + 1} | {recordingLine.duration}
+          </Text>
+          <Button onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+          <Button onPress={() => recordingLine.sound.pauseAsync()} title="Pause"></Button>
+        </View>
+      );
+    });
+
+  }
+  async function clearRecordings() {
+    setRecordingList([])
+  }
 
   const addRecording = () => {
     const newId = audioFiles.length > 0 ? audioFiles[0].id + 1 : 1;
-    setRecordings([{ id: newId, title: "NEWTESTAUDIO" }, ...audioFiles])
+    setRecordingList([{ id: newId, title: "NEWTESTAUDIO" }, ...audioFiles])
   }
 
   const removeRecording = (id: number) => {
-    setRecordings(audioFiles.filter(audioFile => audioFile.id !== id))
+    setRecordingList(audioFiles.filter(audioFile => audioFile.id !== id))
   }
 
   const playRecording = () => {
 
   }
 
-    const pauseRecording = () => {
-    
+  const pauseRecording = () => {
+
   }
 
-  const renderItem = ({ item}) => (
+  const renderItem = ({ item }) => (
     <View style={styles.recordingListed}>
-      <Text 
-      style={[styles.recordingListedText]}>
+      <Text
+        style={[styles.recordingListedText]}>
         {item.title}
       </Text>
       <Pressable onPress={() => playRecording(item.id)}>
@@ -48,18 +102,30 @@ export default function Index() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.newRecordingContainer}>
+      {/* <View style={styles.container}>
+        <Button title={recording ? 'Stop Recording' : 'Start Recording\n\n\n'} onPress={recording ? stopRecording : startRecording} />
+        {getRecordingLines()}
+        <Button title={recordingList.length > 0 ? '\n\n\nClear Recordings' : ''} onPress={clearRecordings} />
+      </View> */}
+
+      <View style={styles.container}>
         <Text style={styles.newRecordingText}>START NEW RECORDING</Text>
-        <Pressable onPress={addRecording}>
-          <Entypo name="controller-record" size={40} color="red" />
+        <Pressable>
+          <Entypo name={recording ? "controller-stop" : "controller-record"} size={40} color="red"
+            onPress={recording ? stopRecording : startRecording} />
+        </Pressable>
+        {getRecordingLines()}
+        <Button title={recordingList.length > 0 ? '\n\n\nClear Recordings' : ''} onPress={clearRecordings} />
+        <Pressable>
+
         </Pressable>
       </View>
-      <FlatList
+      {/* <FlatList
         data={audioFiles}
         renderItem={renderItem}
         keyExtractor={audioFile => audioFile.id.toString()}
-        contentContainerStyle={{flexGrow: 1 }}
-        />
+        contentContainerStyle={{ flexGrow: 1 }}
+      /> */}
     </SafeAreaView>
   );
 }
@@ -70,6 +136,8 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     backgroundColor: 'white',
+    alignItems: 'center', //temp
+    justifyContent: 'center', //temp
   },
   newRecordingContainer: {
     flexDirection: 'row',
@@ -82,11 +150,10 @@ const styles = StyleSheet.create({
     pointerEvents: 'auto',
   },
   newRecordingText: {
-    flex: 1,
+    flexDirection: 'row',
     borderColor: 'white',
     borderWidth: 1,
     borderRadius: 10,
-    padding: 10,
     marginRight: 10,
     fontSize: 20,
     minWidth: 0,
@@ -128,5 +195,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: 'black',
   },
+  fill: {
+    flex: 1,
+    margin: 15,
+  }
 })
 
